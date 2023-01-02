d5eb7ceca44ca059f562a17f24e2b766873f40cb
Geljinskiy <savvageljinskiy@gmail.com>
2023-01-02 02:08:48 -0500

    did hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { ThreeDots } from 'react-loader-spinner';
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { SearchBar } from './Searchbar/Searchbar';
@@ -9,110 +10,205 @@ import { Button } from './Button/Button';
 import '../styles/styles.css';
 import { Modal } from './Modal/Modal';
 
-export class App extends React.Component {
-  state = {
-    currentPage: 1,
-    query: '',
-    images: [],
-    totalPages: 12,
-    canLoadMore: false,
-    status: 'start',
-    modalURL: null,
-  };
+export const App = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [totalPages, setTotalPages] = useState(12);
+  const [canLoadMore, setCanLoadMore] = useState(false);
+  const [status, setStatus] = useState('start');
+  const [modalURL, setModalURL] = useState(null);
 
-  async componentDidUpdate(prevProps, prevState) {
-    const { currentPage, query, totalPages } = this.state;
+  useEffect(() => {
+    async function fetchData() {
+      // if (prevState.query === query && prevState.currentPage === currentPage) {
+      //   return;
+      // }
 
-    //scroll down after adding gallery items
-    window.scrollTo({
-      top: document.body.scrollHeight,
-      behavior: 'smooth',
-    });
-
-    if (prevState.query === query && prevState.currentPage === currentPage) {
-      return;
+      try {
+        //show loading
+        setStatus('load');
+        //getting images from api
+        const { findedImages, totalImages } = await api.getImages(
+          query,
+          totalPages * currentPage,
+          1
+        );
+        //state finded images
+        setImages(findedImages);
+        setCanLoadMore(totalImages > findedImages.length);
+        setStatus(findedImages.length > 0 ? 'success' : 'not found');
+      } catch (error) {
+        console.log('error :', error);
+      }
     }
 
-    try {
-      //show loading
-      this.setState({ status: 'load' });
-      //getting images from api
-      const { findedImages, totalImages } = await api.getImages(
-        query,
-        totalPages * currentPage,
-        1
-      );
-      //state finded images
-      this.setState({
-        images: findedImages,
-        canLoadMore: totalImages > findedImages.length,
-        status: findedImages.length > 0 ? 'success' : 'not found',
+    query && fetchData();
+  }, [query, currentPage, status]);
+
+  useEffect(() => {
+    if (images.length) {
+      //scroll down after adding gallery items
+      window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth',
       });
-    } catch (error) {
-      console.log('error :', error);
-    } finally {
     }
-  }
+  }, [images]);
 
-  onFormSubmit = value => {
+  const onFormSubmit = value => {
     //assign new query and set current page as default
-    this.setState({ query: value, currentPage: 1, images: [] });
+    setQuery(value);
+    setCurrentPage(1);
+    setImages([]);
   };
 
-  onLoadingMore = () => {
-    this.setState(prevState => {
-      return { currentPage: prevState.currentPage + 1 };
-    });
+  const onLoadingMore = () => {
+    setCurrentPage(currentPage + 1);
   };
 
-  openModal = imageURL => {
-    this.setState({ modalURL: imageURL });
+  const openModal = imageURL => {
+    setModalURL(imageURL);
   };
 
-  closeModal = () => {
-    this.setState({ modalURL: null });
+  const closeModal = () => {
+    setModalURL(null);
   };
 
-  render() {
-    const {
-      query,
-      images,
-      totalPages,
-      currentPage,
-      canLoadMore,
-      status,
-      modalURL,
-    } = this.state;
+  return (
+    <div className="App">
+      {modalURL && <Modal largeImageURL={modalURL} closeModal={closeModal} />}
 
-    return (
-      <div className="App">
-        {modalURL && (
-          <Modal largeImageURL={modalURL} closeModal={this.closeModal} />
-        )}
+      <SearchBar onFormSubmit={onFormSubmit} prevQuery={query} />
 
-        <SearchBar onFormSubmit={this.onFormSubmit} prevQuery={query} />
+      <ImageGallery alt={query} images={images} openModal={openModal} />
 
-        <ImageGallery alt={query} images={images} openModal={this.openModal} />
-
-        {status === 'success' && canLoadMore && (
-          <Button
-            onLoadingMore={this.onLoadingMore}
-            totalPages={totalPages}
-            currentPage={currentPage}
-          />
-        )}
-        {status === 'not found' && <h2>Nothing was found...</h2>}
-
-        <ThreeDots
-          height="80"
-          width="80"
-          radius="9"
-          color="#3f51b5"
-          ariaLabel="three-dots-loading"
-          wrapperStyle={{ margin: '0 auto' }}
-          visible={status === 'load'}
+      {status === 'success' && canLoadMore && (
+        <Button
+          onLoadingMore={onLoadingMore}
+          totalPages={totalPages}
+          currentPage={currentPage}
         />
-      </div>
-    );
-  }
-}
+      )}
+      {status === 'not found' && <h2>Nothing was found...</h2>}
+
+      <ThreeDots
+        height="80"
+        width="80"
+        radius="9"
+        color="#3f51b5"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{ margin: '0 auto' }}
+        visible={status === 'load'}
+      />
+    </div>
+  );
+};
+
+// export class App extends React.Component {
+//   state = {
+//     currentPage: 1,
+//     query: '',
+//     images: [],
+//     totalPages: 12,
+//     canLoadMore: false,
+//     status: 'start',
+//     modalURL: null,
+//   };
+
+//   async componentDidUpdate(prevProps, prevState) {
+//     const { currentPage, query, totalPages } = this.state;
+
+//     //scroll down after adding gallery items
+//     window.scrollTo({
+//       top: document.body.scrollHeight,
+//       behavior: 'smooth',
+//     });
+
+//     if (prevState.query === query && prevState.currentPage === currentPage) {
+//       return;
+//     }
+
+//     try {
+//       //show loading
+//       this.setState({ status: 'load' });
+//       //getting images from api
+//       const { findedImages, totalImages } = await api.getImages(
+//         query,
+//         totalPages * currentPage,
+//         1
+//       );
+//       //state finded images
+//       this.setState({
+//         images: findedImages,
+//         canLoadMore: totalImages > findedImages.length,
+//         status: findedImages.length > 0 ? 'success' : 'not found',
+//       });
+//     } catch (error) {
+//       console.log('error :', error);
+//     } finally {
+//     }
+//   }
+
+//   onFormSubmit = value => {
+//     //assign new query and set current page as default
+//     this.setState({ query: value, currentPage: 1, images: [] });
+//   };
+
+//   onLoadingMore = () => {
+//     this.setState(prevState => {
+//       return { currentPage: prevState.currentPage + 1 };
+//     });
+//   };
+
+//   openModal = imageURL => {
+//     this.setState({ modalURL: imageURL });
+//   };
+
+//   closeModal = () => {
+//     this.setState({ modalURL: null });
+//   };
+
+//   render() {
+//     const {
+//       query,
+//       images,
+//       totalPages,
+//       currentPage,
+//       canLoadMore,
+//       status,
+//       modalURL,
+//     } = this.state;
+
+//     return (
+//       <div className="App">
+//         {modalURL && (
+//           <Modal largeImageURL={modalURL} closeModal={this.closeModal} />
+//         )}
+
+//         <SearchBar onFormSubmit={this.onFormSubmit} prevQuery={query} />
+
+//         <ImageGallery alt={query} images={images} openModal={this.openModal} />
+
+//         {status === 'success' && canLoadMore && (
+//           <Button
+//             onLoadingMore={this.onLoadingMore}
+//             totalPages={totalPages}
+//             currentPage={currentPage}
+//           />
+//         )}
+//         {status === 'not found' && <h2>Nothing was found...</h2>}
+
+//         <ThreeDots
+//           height="80"
+//           width="80"
+//           radius="9"
+//           color="#3f51b5"
+//           ariaLabel="three-dots-loading"
+//           wrapperStyle={{ margin: '0 auto' }}
+//           visible={status === 'load'}
+//         />
+//       </div>
+//     );
+//   }
+// }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,43 +1,74 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 
 import '../../styles/styles.css';
 
-export class Modal extends React.Component {
-  componentDidMount() {
-    document.body.style.overflowY = 'hidden';
-    window.addEventListener('keydown', this.onKey);
-  }
+export const Modal = ({ largeImageURL, closeModal }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', onKey);
 
-  componentWillUnmount() {
-    document.body.style.overflowY = 'unset';
-    window.removeEventListener('keydown', this.onKey);
-  }
+    return () => {
+      window.removeEventListener('keydown', onKey);
+    };
+  }, []);
 
-  onKey = ev => {
+  function onKey(ev) {
     if (ev.code === 'Escape') {
-      this.props.closeModal();
+      closeModal();
     }
-  };
+  }
 
-  onBackdropClick = ev => {
+  function onBackdropClick(ev) {
     const { currentTarget, target } = ev;
     if (currentTarget === target) {
-      this.props.closeModal();
+      closeModal();
     }
-  };
-
-  render() {
-    const { largeImageURL } = this.props;
-    return (
-      <div onClick={this.onBackdropClick} className="Overlay">
-        <div className="Modal">
-          <img src={largeImageURL} alt="" />
-        </div>
-      </div>
-    );
   }
-}
+  return (
+    <div onClick={onBackdropClick} className="Overlay">
+      <div className="Modal">
+        <img src={largeImageURL} alt="" />
+      </div>
+    </div>
+  );
+};
+
+// export class Modal extends React.Component {
+//   componentDidMount() {
+//     document.body.style.overflowY = 'hidden';
+//     window.addEventListener('keydown', this.onKey);
+//   }
+
+//   componentWillUnmount() {
+//     document.body.style.overflowY = 'unset';
+//     window.removeEventListener('keydown', this.onKey);
+//   }
+
+//   onKey = ev => {
+//     if (ev.code === 'Escape') {
+//       this.props.closeModal();
+//     }
+//   };
+
+//   onBackdropClick = ev => {
+//     const { currentTarget, target } = ev;
+//     if (currentTarget === target) {
+//       this.props.closeModal();
+//     }
+//   };
+
+//   render() {
+//     const { largeImageURL } = this.props;
+//     return (
+//       <div onClick={this.onBackdropClick} className="Overlay">
+//         <div className="Modal">
+//           <img src={largeImageURL} alt="" />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 Modal.propTypes = {
   largeImageURL: PropTypes.string.isRequired,
